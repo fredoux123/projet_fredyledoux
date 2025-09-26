e4273f6b89a1c04221c9d93c7ac11f86e724f38e
fredoux123 <fredylapache@gmail.com>
2025-09-26 10:08:55 -0400

    initialisation du projet_fredyLedoux

---
+++ src/main/java/org/demos/projet_fredyledoux/HelloApplication.java
@@ -0,0 +1,9 @@
+package org.demos.projet_fredyledoux;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
